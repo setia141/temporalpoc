4dd97b02a4846365e592c0bb06686b7ea8977cbc
Deepak Setia <deepaksetia141@gmail.com>
2024-11-20 00:33:28 -0500

    First commit

---
+++ src/main/java/io/temporal/samples/springboot/TemporalSpringbootSamplesApplication.java
@@ -0,0 +1,11 @@
+package io.temporal.samples.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemporalSpringbootSamplesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TemporalSpringbootSamplesApplication.class, args);
+	}
+}
